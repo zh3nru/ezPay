901ef8a4b913ecaa38b9dd13fc84b9b2a5b8f7cb
John Christopher Cerita <cerita.173455@gensan.sti.edu.ph>
2024-07-08 12:42:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/e_money/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.e_money;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+    CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.et2);
+        checkBox = (CheckBox) findViewById(R.id.cb);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    editText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+                else {
+                    editText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+
+        button = (Button) findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
